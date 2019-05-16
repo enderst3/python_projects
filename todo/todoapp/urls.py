1c11ca9446b2a39905d9ebdae6854280d441faf3
enderst3 <enderst3@gmail.com>
2019-05-16 10:38:54 -0700

    display data

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -5,5 +5,5 @@ from todoapp import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-
+    url(r'^(?P<task_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is the todoapp homepage')
+    task_list = Task.objects.all()
+    output = ','.join([task.task_name for task in task_list])
+    return HttpResponse(output)
 
+def detail(request, task_id):
+    return HttpResponse("This is task number {}".format(task_id))
