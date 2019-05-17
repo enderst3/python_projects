c4b3e14b84c390ca00f28d9a40cdf63268dfdd10
enderst3 <enderst3@gmail.com>
2019-05-17 14:44:17 -0700

    templates

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     task_list = Task.objects.all()
-    output = ','.join([task.task_name for task in task_list])
-    return HttpResponse(output)
+    # pass template to file
+    context = {
+        'task_list':task_list
+    }
+    return render(request, 'todoapp/index.html', context)
 
 def detail(request, task_id):
-    return HttpResponse("This is task number {}".format(task_id))
+    task = Task.objects.get(pk=task_id)
+    context = {
+        'task':task
+    }
+    return render(request, 'todoapp/detail.html', context)
