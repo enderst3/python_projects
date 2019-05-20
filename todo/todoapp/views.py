49416a7fed59db9bb289ed9e42ceac6f13a4f9a1
enderst3 <enderst3@gmail.com>
2019-05-19 21:48:19 -0700

    404 error

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Task
 from django.template import loader
 
@@ -14,7 +14,10 @@ def index(request):
     return render(request, 'todoapp/index.html', context)
 
 def detail(request, task_id):
-    task = Task.objects.get(pk=task_id)
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404('Task does not exist')
     context = {
         'task':task
     }
