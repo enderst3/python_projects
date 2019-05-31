54e0726436fd6863fdab4d3554e3e7fcb0839a15
enderst3 <enderst3@gmail.com>
2019-05-30 20:50:25 -0700

    dummy data

--- home_finder_app/homesapp/admin.py
+++ home_finder_app/homesapp/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Property, Location
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Property)
+admin.site.register(Location)
