f617b9f2f8d46d53a2b2f91fdd3182de713817b8
enderst3 <enderst3@gmail.com>
2019-05-31 20:32:15 -0700

    index view

--- home_finder_app/homesapp/views.py
+++ home_finder_app/homesapp/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views import generic
+from .models import Location
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'homesapp/index.html'
+
+    def get_queryset(self):
+        return Location.objects.all()
